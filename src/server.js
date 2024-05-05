7e6a2249b969bc8099d21b389e9113bf35558895
Ingrid <oligrid20@gmail.com>
2024-05-05 16:23:04 -0300

    feat: Added CORS configuration

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import {routes} from "./Routes/index.route.js";
 import {errorHandler} from "./Middlewares/error/erro.middleware.js";
 import { testConnection } from "./database/connection.js";
-
+import cors from "cors"
 const app = express();
 const port = 4979;
 app.use(express.json());
+app.use(cors({origin: "*"}))
 app.use(routes);
 app.use(errorHandler);
 
